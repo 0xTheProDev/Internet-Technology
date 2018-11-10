5d910155ad3f36c21cb839f626d0c6217d8e5e05
Progyan Bhattacharya <progyanb@acm.org>
2018-11-10 15:50:58 +0530

    Perl: String Variations
    Single quoted non interpolating string and double quoted interpolating string.

---
+++ Perl/Quote.pl
@@ -0,0 +1,10 @@
+#! /usr/bin/perl
+
+print "It's an interpolated string\n";
+print 'This is a non "interpolated" string', "\n";
+print qq/This is a 'quote' like string.\n/;
+print<<EOF;
+
+This is a Here Document.
+
+EOF
