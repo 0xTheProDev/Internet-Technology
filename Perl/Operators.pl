6be343d518e46415ddb10177bce739a07be58ec0
Progyan Bhattacharya <progyanb@acm.org>
2018-11-10 16:06:41 +0530

    Perl: Arithmetic Operators

---
+++ Perl/Operators.pl
@@ -0,0 +1,19 @@
+#! /usr/bin/perl
+
+# Arithmetic Operations
+# Power Operation
+print "2 ** 4 = ", 2 ** 4, "\n";
+
+# Unary Minus Operation
+print "-2 ** 4 = ", -2 ** 4, "\n";
+
+# Division/Multiplication Operation
+print "3 * 2 ** 2 = ", 3 * 2 ** 2, "\n";
+print "64 / -4 ** 2 = ", 64 / -4 ** 2, "\n";
+
+# Modulo Operation
+print "36 % 5 = ", 36 % 5, "\n";
+
+# Addition/Subtraction Operation
+print "5 * 4 + 2 = ", 5 * 4 + 2, "\n";
+print "10 / 2 - 2 = ", 10 / 2 - 2, "\n";
